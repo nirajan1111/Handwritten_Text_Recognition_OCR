3ae49b108c2b3d99a45d0902c66e1877bb6eeb67
nirajan1111 <nirajansah1111@gmail.com>
2024-09-23 15:11:45 +0545

    basic level project done

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+import React from 'react'
+import ImageUploader from './upload'
+
+
+const page = () => {
+
+React.useEffect(() => {
+  document.addEventListener('contextmenu', event => event.preventDefault());
+
+}, [])
+  return (
+    <div className='flex-col m-auto justify-center mt-7 '>
+      <h1 className=' text-center m-auto'>Upload ur  assignment</h1>
+      <ImageUploader />
+      
+
+    </div>
+  )
+}
+
+export default page
